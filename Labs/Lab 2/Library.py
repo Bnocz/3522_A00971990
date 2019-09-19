3cb409cabb462a9cca57cfc79c805bd3c2288cc8
Berty <BertNocon2@gmail.com>
2019-09-18 18:48:56 -0700

    Lab02: Implemented and tested check_out and return_book methods

--- Labs/Lab 2/Library.py
+++ Labs/Lab 2/Library.py
@@ -16,21 +16,30 @@ class Library:
             self.book_list.append(new_book)
 
     def check_out(self, call_number):
-        pass
+        for Book in self.book_list:
+            if call_number == Book.call_number and Book.num_copies >= 1:
+                Book.num_copies -= 1
+                print(f"Your book has been checked out, there are {Book.num_copies} left")
+            elif call_number == Book.call_number and Book.num_copies == 0:
+                print("Sorry that book is unavailable right now")
+
     def return_book(self, call_number):
-        pass
+        for Book in self.book_list:
+            if call_number == Book.call_number:
+                Book.num_copies += 1
+                print(f"thanks for returning your book, there are now {Book.num_copies} left!")
+
     def display_available_books(self):
         pass
 
 
 def main():
-    LoTR = book.Book("Lord of the Rings", 1023, "JRR Tolkien", 1)
+    LoTR = book.Book("Lord of the Rings", "1023", "JRR Tolkien", 1)
     GoT = book.Book("Game of Thrones", 1033, "GRR Martin", 1)
     HP = book.Book("Harry Potter", 1212, "JK Rowling", 1)
     book_list = [LoTR, GoT]
     artemis = Library(book_list)
-    artemis.add_book(GoT)
-    print(artemis.book_list)
+    artemis.return_book(1033)
 
 if __name__ == '__main__':
     main()
