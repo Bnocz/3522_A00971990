4a754bb2fe0dd199ed4764acf964bbe7300c8cd6
Bnocz <bertnocon2@gmail.com>
2019-09-21 11:09:36 -0700

    added speak methods

--- Assignments/Assignment 1/controller.py
+++ Assignments/Assignment 1/controller.py
@@ -9,11 +9,11 @@ class Controller:
         if choice == 1:
             tamagotchispawner = random.randint(1, 3)
             if tamagotchispawner == 1:
-                tama = Goku(100, 100, 0)
+                tama = tamagotchi.Goku(100, 100, 0)
             if tamagotchispawner == 2:
-                tama = Yamcha(100, 100, 0)
+                tama = tamagotchi.Yamcha(100, 100, 0)
             if tamagotchispawner == 3:
-                tama = Krillin(100, 100, 0)
+                tama = tamagotchi.Krillin(100, 100, 0)
             while True:
                 print(tama)
                 choice = int(input("What do you want to do? "))
--- Assignments/Assignment 1/tamagotchi.py
+++ Assignments/Assignment 1/tamagotchi.py
@@ -7,25 +7,22 @@ class Tamagotchi:
         self.hunger = hunger
 
     def raisehunger(self):
-        starttime = time.time()
-        while True:
-            self.hunger += 1
-            time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+        self.hunger = 100
 
     def lowerhunger(self):
-        self.hunger == 0
+        pass
 
     def lowerhealth(self):
         pass
 
     def raisehealth(self, health):
-        self.health += health
+        self.health = 100
 
     def lowerhappiness(self):
         pass
 
     def raisehappiness(self, happiness):
-        self.happiness += happiness
+        self.happiness = 100
 
     def speak(self):
         pass
@@ -33,12 +30,32 @@ class Tamagotchi:
     def die(self):
         pass
 
+    def check_status(self):
+
+
     def __str__(self):
         return f"Hi I'm  and my hunger is {self.hunger}"
 
 class Goku(Tamagotchi):
     def __init__(self, happiness, health, hunger):
         super().__init__(100, 100, 0)
+
+    def speak(self):
+        if self.hapiness<= 50:
+            print("I'm feeling kinda down")
+        if self.happiness <= 20:
+            print("...")
+        if self.hunger <= 50:
+            print("I'm really starting to get hungry!")
+        if self.hunger <= 20:
+            print("I'm dying of hunger!")
+        if self.health <= 50:
+            print("*sniff* my nose is stuffed")
+        if self.health <= 20:
+            print("I think i need to see a doctor")
+        else:
+            print("Wonder how Gohan is doing..")
+
 
 
     def __str__(self):
@@ -52,6 +69,21 @@ class Yamcha(Tamagotchi):
     def __str__(self):
         return f"Hi I'm yamcha and my hunger is {self.hunger}"
 
+    def speak(self):
+        if self.hapiness<= 50:
+            print("I'm feeling kinda down")
+        if self.happiness <= 20:
+            print("...")
+        if self.hunger <= 50:
+            print("I'm really starting to get hungry!")
+        if self.hunger <= 20:
+            print("I'm dying of hunger!")
+        if self.health <= 50:
+            print("*sniff* my nose is stuffed")
+        if self.health <= 20:
+            print("I think i need to see a doctor")
+        else:
+            print("Wonder how Gohan is doing..")
 
 class Krillin(Tamagotchi):
     def __init__(self, happiness, health, hunger):
@@ -59,6 +91,24 @@ class Krillin(Tamagotchi):
 
     def __str__(self):
         return f"Hi I'm Krillin and my hunger is {self.hunger}"
+
+    def speak(self):
+        if self.hapiness <= 50:
+            print("I'm feeling kinda down")
+        if self.happiness <= 20:
+            print("...")
+        if self.hunger <= 50:
+            print("I'm really starting to get hungry!")
+        if self.hunger <= 20:
+            print("I'm dying of hunger!")
+        if self.health <= 50:
+            print("*sniff* my nose is stuffed")
+        if self.health <= 20:
+            print("I think i need to see a doctor")
+        else:
+            print("Wonder how Gohan is doing..")
+
+
 def main():
 
     n = int(input("enter a number"))
