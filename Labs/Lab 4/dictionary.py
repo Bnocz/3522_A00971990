3ce840ec4c8c2cf3848ead667f67cac0fa0e2ceb
Berty <BertNocon2@gmail.com>
2019-10-03 21:37:51 -0700

    Dictionary allows you to search words and is not case-sensitive, dictionary loads up on start from file handler

--- Labs/Lab 4/dictionary.py
+++ Labs/Lab 4/dictionary.py
@@ -19,11 +19,17 @@ class InvalidKeyError(Exception):
 def main():
     jdict_ = Dictionary_()
     jdict_ = file_handler.FileHandler.load_data("testdict.json")
-    search_word = input("Enter a word to search for: ")
-    try:
-        Dictionary_.word_search(jdict_, search_word)
-        if search_word not in jdict_:
-    except InvalidKeyError:
+    count = 0
+    search_word = ""
+    print(f"Enter a word you would like to search the definition for, enter quitprogram to quit")
+    while search_word != 'quitprogram':
+        search_word = input(">")
+        Dictionary_.word_search(jdict_, search_word.lower())
+        if search_word.lower not in jdict_:
+            Dictionary_.word_search(jdict_, search_word.upper())
+        else:
+            print("Sorry, we don't have that definition.")
+
 
 if __name__ == '__main__':
     main()
