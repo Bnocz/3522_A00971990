8f69c1aa83add9f7615fd46f9d525558b51fb713
Berty <BertNocon2@gmail.com>
2019-10-03 22:05:49 -0700

    Dictionary allows you to search words and is not case-sensitive, dictionary loads up on start from file handler

--- Labs/Lab 4/dictionary.py
+++ Labs/Lab 4/dictionary.py
@@ -1,24 +1,26 @@
 import json
 import file_handler
+from file_handler import *
 class Dictionary_:
 
-    @staticmethod
-    def testopen():
-        with open("testdict.json", "r") as file:
-            datastore = json.load(file)
-            print(datastore)
     @staticmethod
     def word_search(word_dict, word_search):
         for key, value in word_dict.items():
             if word_search == key:
                 print(value)
 
-class InvalidKeyError(Exception):
-    pass
-
 def main():
+    '''
+    Creates a Dictionary object, calls load_data to populate dictionary from
+    a file. Allows user to search for words and returns their definition.
+
+    :return:
+    '''
     jdict_ = Dictionary_()
-    jdict_ = file_handler.FileHandler.load_data("testdict.json")
+    print(f"Enter the name of the file you would like to open")
+    file_path = input(">")
+
+    jdict_ = file_handler.FileHandler.load_data(file_path)
     count = 0
     search_word = ""
     print(f"Enter a word you would like to search the definition for, enter quitprogram to quit")
@@ -26,9 +28,10 @@ def main():
         search_word = input(">")
         Dictionary_.word_search(jdict_, search_word.lower())
         if search_word.lower not in jdict_:
-            Dictionary_.word_search(jdict_, search_word.upper())
+             Dictionary_.word_search(jdict_, search_word.upper())
         else:
-            print("Sorry, we don't have that definition.")
+            print("Sorry we don't have that definition")
+
 
 
 if __name__ == '__main__':
