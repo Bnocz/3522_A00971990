5043523f6bcfd97d50dbd3a1904eb7a7ac1d6382
Berty <BertNocon2@gmail.com>
2019-10-14 22:15:28 -0700

    added ability to add, remove, and search cards. Also created method to search by type but haven't implemented menu yet

--- Assignments/Assignment 2/card.py
+++ Assignments/Assignment 2/card.py
@@ -4,107 +4,177 @@ import unicodedata
 
 class Card(abc.ABC):
     default_payment_data = {
-        'Name': None,
-        'Bank': None,
-        'CardNum': None,
-        'Expires': None
+        'name': None,
+        'card_face_name': "",
+        'bank': None,
+        'number': "",
+        'expires': None,
+        'optional': ""
     }
-    @abc.abstractmethod
+
     def __init__(self, **kwargs):
-        pass
+        self.card_data = Card.default_payment_data
+        for key, item in kwargs.items():
+            setattr(self, key, item)
 
-    @abc.abstractmethod
     def __getitem__(self, key):
-        pass
+        return self.card_data[key]
 
-    @abc.abstractmethod
     def display_data(self, **kwargs):
-        pass
+        if self.optional != "":
+            print(f"Name: {self.name}")
+            print(f"Issuer: {self.bank}")
+            if self.number != "":
+                print(f"Number: {self.number}")
+            print(f"Card Type: {self['card_type']}")
+            print(f"Notes: {self.optional}\n")
+        else:
+            print(f"Name: {self.name}")
+            print(f"Issuer: {self.bank}")
+            if self.number != "":
+                print(f"Number: {self.number}")
 
     def __str__(self):
         return f"Nickname: {self['Name']}, Bank: {self['Bank']}, " \
         f"CardNum: {self['CardNum']}, Expires on: {self['Expires']}"
 
-class PaymentCard(Card):
+class IdCard(Card):
 
-    def __init__(self, **kwargs):
-        self.card_data = dict(Card.default_payment_data)
-        self.card_data = Card.default_payment_data
-        for key, item in kwargs.items():
-            self.card_data[key] = item
+    def __int__(self, **kwargs):
+        super().__init__()
 
     def __getitem__(self, key):
-        return self.card_data[key]
+        super().__getitem__()
 
     def display_data(self, **kwargs):
-        if self['optional'] != "":
-            print(f"Name: {self['Name']}")
-            print(f"Issuer: {self['Bank']}")
-            print(f"Card Number: {self['Number']}")
-            print(f"Expires on: {self['Expires']}")
-            print(f"Notes: {self['optional']}")
-            print(f"Card Type: {self['Card_Type']}")
+        if self.optional != "":
+            print(f"Card Name: {self.name}")
+            print(f"Name on Card: {self.card_face_name}")
+            print(f"Issuer: {self.bank}")
+            print(f"Card Number: {self.number}")
+            print(f"Expires on: {self.expires}")
+            print(f"Card Type: {self['card_type']}")
+            print(f"Notes: {self.optional}\n")
         else:
-            print(f"Name: {self['Name']}")
-            print(f"Bank: {self['Bank']}")
-            print(f"Card Number: {self['CardNum']}")
-            print(f"Expires on: {self['Expires']}")
-            print(f"Card Type: {self['Card_Type']}")
+            print(f"Card Name: {self.name}")
+            print(f"Name on Card: {self.card_face_name}")
+            print(f"Issuer: {self.bank}")
+            print(f"Card Number: {self.number}")
+            print(f"Expires on: {self.expires}\n")
 
     def __str__(self):
-        return f"{self['kwargs']}"
+        return f"{self['Name']}"
+
+
+class CreditCard(IdCard):
+    def __int__(self, **kwargs):
+        super().__init__()
+
+    def __getitem__(self, key):
+        super().__getitem__()
+
+    def display_data(self, **kwargs):
+        super().display_data()
+
+    def __str__(self):
+        return f"{self['Name']}"
+
+
+class DebitCard(IdCard):
+    def __int__(self, **kwargs):
+        super().__init__()
+
+    def __getitem__(self, key):
+        super().__getitem__()
+
+    def display_data(self, **kwargs):
+        super().display_data()
+
+    def __str__(self):
+        return f"{self['Name']}"
 
 class LoyaltyCard(Card):
-
-    def __init__(self, **kwargs):
-        self.card_data = dict(Card.default_payment_data)
-        self.card_data = Card.default_payment_data
-        for key, item in kwargs.items():
-            self.card_data[key] = item
+    def __int__(self, **kwargs):
+        super().__init__()
 
     def __getitem__(self, key):
-        return self.card_data[key]
+        super().__getitem__()
 
     def display_data(self, **kwargs):
-        if self['Number'] != "":
-            print(f"Name: {self['Name']}")
-            print(f"Issuer: {self['Bank']}")
-            print(f"Card Number: {self['Number']}")
-            print(f"Notes: {self['optional']}")
-            print(f"Card Type: {self['Card_Type']}")
-        else:
-            print(f"Name: {self['Name']}")
-            print(f"Bank: {self['Bank']}")
-            print(f"Notes: {self['optional']}")
-            print(f"Card Type: {self['Card_Type']}")
+        super().display_data()
+
 
     def __str__(self):
-        return str(f"{self.name}: {self.kwargs}")
+        return f"{self['Name']}"
 
-
-
-class MiscCard(Card):
-
-    def __init__(self, **kwargs):
-        self.card_data = dict(Card.default_payment_data)
-        self.card_data = Card.default_payment_data
-        for key, item in kwargs.items():
-            self.card_data[key] = item
+class BusinessCard(Card):
+    def __int__(self, **kwargs):
+        super().__init__()
 
     def __getitem__(self, key):
-        return self.card_data[key]
+        super().__getitem__()
+
+    def display_data(self, **kwargs):
+
+        if self.optional != "":
+            print(f"Card Name: {self.name}")
+            print(f"Issuer name: {self.bank}")
+            if self.number != "":
+                print(f"Number: {self.number}")
+            print(f"Card Type: {self['card_type']}")
+            print(f"Notes: {self.optional}\n")
+        else:
+            print(f"Name: {self.name}")
+            print(f"Issuer: {self.bank}")
+            if self.number != "":
+                print(f"Number: {self.number}")
+
+    def __str__(self):
+        return f"{self['Name']}"
+
+
+
+class PersonalIdentificationCard(IdCard):
+
+    def __int__(self, **kwargs):
+        super().__init__()
+
+    def __getitem__(self, key):
+        super().__getitem__()
+
+    def display_data(self, **kwargs):
+        super().display_data()
+
+    def __str__(self):
+        return f"{self['Name']}"
+
+
+class OtherCard(Card):
+    def __int__(self, **kwargs):
+        super().__init__()
+
+    def __getitem__(self, key):
+        super().__getitem__()
+
+    def display_data(self, **kwargs):
+        super().display_data()
+
+    def __str__(self):
+        return f"{self['Name']}"
 
 
 class CardGenerator:
     @staticmethod
     def generate_card(**kwargs):
         for key, value in kwargs.items():
-            card = PaymentCard(key, value)
+            card = IdCard(key, value)
 
     @staticmethod
-    def generate_payment_card(card_type, name, issuer):
+    def generate_id_card(card_type, name, issuer):
         user_input = None;
         company = issuer
+        user_input = input("Enter the name on the card: ")
+        card_face_name = user_input
         user_input = input("Enter the card number: ")
         card_number = user_input
         user_input = input("Enter the expiry date(MM/DD): ")
@@ -112,11 +182,16 @@ class CardGenerator:
         user_input = input("Would you like to enter a note? (Leave blank if you don't want a note): ")
         optional = user_input
         if card_type == "debit":
-            card_type = "Debit Card"
+            generated_card = DebitCard(name=name, card_face_name=card_face_name, bank=company, number=card_number,
+                                       expires=expiry_date, optional=optional)
         elif card_type == "credit":
-            card_type = "Credit Card"
-        generated_card = PaymentCard(Name=name, Bank=company, Number=card_number,
-                                     Expires=expiry_date, Card_Type=card_type, optional=optional)
+            generated_card = CreditCard(name=name, card_face_name=card_face_name, bank=company, number=card_number,
+                                        expires=expiry_date, optional=optional)
+        else:
+            generated_card = PersonalIdentificationCard(name=name, card_face_name=card_face_name,
+                                                        bank=company, number=card_number,
+                                                        expires=expiry_date, optional=optional)
+
         return generated_card
 
     @staticmethod
@@ -128,38 +203,47 @@ class CardGenerator:
         user_input = input("Would you like to enter a note? (Leave blank if you don't want a note): ")
         optional = user_input
         if card_type == "loyalty":
-            card_type = "Loyalty Card"
-            generated_card = LoyaltyCard(Name=name, Bank=company, Number=card_number,
-                                         Card_Type=card_type, optional=optional)
+            generated_card = LoyaltyCard(name=name, bank=company, number=card_number,
+                                         optional=optional)
         elif card_type == "business":
             card_type = "Business Card"
-            generated_card = MiscCard(Name=name, Bank=company, Number=card_number,
-                                      Card_Type=card_type, optional=optional)
+            generated_card = BusinessCard(name=name, bank=company, number=card_number,
+                                         optional=optional)
         else:
             user_input = input("Enter the card type: ")
             card_type = user_input
-            generated_card = MiscCard(Name=name, Bank=company, Number=card_number,
-                                      Card_Type=card_type, optional=optional)
+            generated_card = OtherCard(name=name, bank=company, number=card_number,
+                                         optional=optional, card_type=card_type)
 
         return generated_card
 
     @staticmethod
     def generateCard():
-        user_input = None;
+        user_input = None
+        new_card = None
         print("Please enter a nickname for your card: ")
         user_input = input(">")
         name = user_input
-        print("Who is the card issuer?")
+        print("Who is the card issuer?(Or name, for business cards)")
         user_input = input(">")
         issuer = user_input
-        print(f"What kind of card is {name}? (Credit, Debit, Loyalty, Business, Other")
-        user_input = input(">")
-        user_input = user_input.casefold()
-        if user_input == "credit" or user_input == "debit":
-            new_card = CardGenerator.generate_payment_card(user_input.casefold(), name, issuer)
-        elif user_input == "other" or user_input == 'loyalty' or user_input == 'business':
-            new_card = CardGenerator.generate_other_card(user_input.casefold().replace(" ", ""), name, issuer)
-        return new_card
+        while True:
+            try:
+                print(f"What kind of card is {name}? (Credit, Debit, Loyalty, Business, Other")
+                user_input = input(">")
+                user_input = user_input.casefold()
+                if user_input == "credit" or user_input == "debit"or user_input == 'id':
+                    new_card = CardGenerator.generate_id_card(user_input.casefold(), name, issuer)
+                    return new_card
+                elif user_input == "other" or user_input == 'loyalty' or \
+                        user_input == 'business':
+                    new_card = CardGenerator.generate_other_card(user_input.casefold().replace(" ", ""), name, issuer)
+                    return new_card
+                else:
+                    raise ValueError
+            except ValueError:
+                print("Sorry, that's an invalid input")
+
     @staticmethod
     def add_cards():
         new_card = CardGenerator.generateCard()
--- Assignments/Assignment 2/wallet.py
+++ Assignments/Assignment 2/wallet.py
@@ -1,8 +1,10 @@
 from card import *
+import json
 class Wallet:
 
     def __init__(self, card_list):
         self.card_list = card_list
+
 
     def add_card(self):
         new_card = CardGenerator.generateCard()
@@ -10,30 +12,87 @@ class Wallet:
         print("Successfully added: ")
         new_card.display_data()
 
+    def display_wallet_by_type(self, type_):
+        x = [card.display_data() if type(card) is type_ else '' for card in self.card_list]
+
+    def display_wallet(self):
+        for card in self.card_list:
+            print(card.name)
+
+    def search_wallet_by_name(self, query):
+        search_result = self._retrieve_card_by_name(query)
+        search_result.display_data()
+
+    def search_wallet_by_number(self, query):
+        search_result = self._retrieve_card_by_number(query)
+        search_result.display_data()
+
     def _retrieve_card_by_name(self, name):
         requested_card = None
-        requested_card = next(name for card in self.card_list if card['Name'] == name)
+        requested_card = next((item for item in self.card_list if item.name == name), None)
+        return requested_card
+
+    def _retrieve_card_by_number(self, number):
+        requested_card = None
+        requested_card = next((item for item in self.card_list if item.number == number), None)
         return requested_card
 
     def remove_card(self, name):
         requested_card = self._retrieve_card_by_name(name)
         self.card_list.remove(requested_card)
-        print("Card removed: ")
-        for card in self.card_list:
-            print(card)
+        print(f"Removed {requested_card.name} from wallet")
+
+    def write_to_file(self):
+        with open("testfile.txt", 'w') as testfile:
+            for i in self.card_list:
+                testfile.write(json.dumps(self.card_list))
 
 
-def generateTestCards():
-    test_wallet = [
-        PaymentCard(name="Apple", Bank="Visa", Number=54654564, Expires="01/20", Card_Type="Credit Card", business='Oppa'),
-        PaymentCard(name="Orange", Bank="Mastercard", Number=21313213, Expires="01/20", Card_Type="Debit Card"),
-        ]
-    return test_wallet
+
+def generate_test_cards():
+    card1 = CreditCard(name="Orange", card_face_name='orangutang',bank="Mastercard", number="21313213", expires="01/20", optional="")
+    card2 = IdCard(name="Cherry", card_face_name='Potato Boy', bank="Mastercard", number="45489841", expires="09/21", optional="")
+    card3 = LoyaltyCard(name='Banana', card_face_name='Jell-O', bank="Indigo", number="", card_type="Loyalty", optional="")
+
+
+    test_cards = [
+        card1, card2, card3
+    ]
+
+    return test_cards
 
 def main():
-    test_cards = generateTestCards()
+    test_cards = generate_test_cards()
     test_wallet = Wallet(test_cards)
-    test_wallet.remove_card("Apple")
+    user_input = None;
+    while user_input != "q":
+        print("Type add to add a card")
+        user_input = input('>')
+        if user_input == 'add':
+            test_wallet.add_card()
+        elif user_input == 'display':
+            print(f"Enter 1 to view all Credit Cards")
+            print(f"Enter 2 to view all Debit Cards")
+            print(f"Enter 3 to view all Loyalty Cards")
+        elif user_input == 'search':
+            print("would you like to search by Card Name or Number?")
+            user_input = input(">")
+            if user_input == 'name':
+                user_query = None;
+                print("enter the card name")
+                user_query = input('>')
+                test_wallet.search_wallet_by_name(user_query)
+            elif user_input == 'number':
+                user_query = None;
+                print("enter the card number")
+                user_query = input('>')
+                test_wallet.search_wallet_by_number(user_query)
+            else:
+                continue
+
+
+
+
 
 
 if __name__ == '__main__':
